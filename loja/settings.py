217137ce52425b4d50cebeb843a5161f29811a9b
rodrigo <rodrigog3wconcept@gmail.com>
2025-05-08 16:35:57 -0300

    created the models and Produto and Variação

--- loja/settings.py
+++ loja/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'produto',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
