71105f4a5b1130ce34b20d33db1a85dc0e77cec4
rodrigo <rodrigog3wconcept@gmail.com>
2025-06-10 16:36:24 -0300

    created saving order pt 1

--- pedido/models.py
+++ pedido/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Pedido(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     total =  models.FloatField()
+    qtd_total = models.PositiveIntegerField()
     status = models.CharField(
         default='C',
         max_length=1,
@@ -41,3 +42,4 @@ class ItemPedido(models.Model):
         class Meta:
              verbose_name = 'Item do pedido'
              verbose_name_plural = 'Itens do pedido'
+
